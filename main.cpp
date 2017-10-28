8b19c2a0d333bc2407c5cc7b4b2aba4e9e081456
ParksideAdrian <32446689+ParksideAdrian@users.noreply.github.com>
2017-10-28 10:57:05 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,14 +9,25 @@
 #include <iostream>
 using namespace std;
 const double PER_MILE = 0.58;
+double carCost;
 
-
+void carRental(double &refVar1);
+void costPerMile(double &refVar);
+void airFair();
+void tripLength();
+void conferenceFees();
 void carRental();
 void costPerMile();
+void departureTime();
+void checkTime();
 
 int main(){
-    
-    carRental();
+    tripLength();
+    departureTime();
+    airFair();
+    double carCost;
+    carRental(carCost);
+    conferenceFees();
     
     return 0;
 }
@@ -27,51 +38,150 @@ int main(){
  
  **********************************************/
 
-void carRental(){
+void carRental(double &refVar1){
     char ch;
-    double carCost;
     char privateCar;
+
     
     cout << "Did you rent a car? (y/n): ";
     cin >> ch;
         if (ch == 'y' || ch == 'Y'){
             cout << "Enter cost of the rental car: ";
-            cin >> carCost;
+            cin >> refVar1;
             }
         else if(ch == 'n' || ch =='N'){
             cout << "Was a private vehicle used? (y/n): ";
             cin >> privateCar;
             
             if( privateCar == 'y' ||  privateCar == 'Y' ){
-            costPerMile();
+            costPerMile(refVar1);
             }
             else if(privateCar == 'n' || privateCar == 'N'){
-                carCost = 0;
+                refVar1 = 0;
             }
         else if (ch != 'Y' || ch!= 'y' || ch != 'N' || ch != 'n'){
             cout << "Error. Invalid input, please try again";
             cin >> ch;
             }
         }
-}
+    cout << "Car cost is: " << refVar1 << " Car cost reimbursed: " << refVar1 << endl;
+    cout << endl;
+    }
+    
+    
 
 /************************************
  
  Miles Driven
- Vehicle allowance is $.58 per mile.
+ Vehicle allowance is $0.58 per mile.
  
  ************************************/
 
-void costPerMile(){
-    
-    double carCost;
-    
+void costPerMile(double &refVar){
     int milesDriven;
     
     cout << "How many miles were driven? ";
     cin >> milesDriven;
     
-    carCost = milesDriven * PER_MILE;
+    refVar = milesDriven * PER_MILE;
     
-    cout << "Total is: $ " << carCost << endl;
 }
+
+void tripLength()
+{
+    int days;
+    cout << "Amount of days on trip: ";
+    cin >> days;
+    cout << endl;
+}
+
+void airFair()
+{
+    double cost, reimbursement;
+    cout << "Enter cost of round trip: ";
+    cin >> cost;
+    reimbursement = cost;
+    cout << "Flight cost: " << cost << " " << "Flight cost reimbursed: " << reimbursement << endl;
+    cout << endl;
+}
+
+void conferenceFees()
+{
+    double conferenceFee, reimbursement;
+    cout << "Enter cost of conference registration fees: ";
+    cin >> conferenceFee;
+    reimbursement = conferenceFee;
+    cout << "Conference fees: "<< conferenceFee << " Conference fees reimbursed: " << reimbursement << endl;
+    cout << endl;
+}
+
+/*****************************************************************
+ *              Departure Time and Return Time                    *
+ *                                                                *
+ *    This Function receives information on the departure time    *
+ *         and the return time for the business person            *
+ *****************************************************************/
+void departureTime()
+{
+    double timeDepart,
+    timeReturn;
+    
+    cout << "What time was Departure (Military Time): ";
+    cin  >> timeDepart;
+    while(timeDepart < 0 || timeDepart > 2459){
+        cout << "ERROR! Invalid Time! Re-Enter Time: ";
+        cin  >> timeDepart;
+    }
+    
+    cout << "What time was the arrival (Military Time): ";
+    cin  >> timeReturn;
+    while(timeReturn < 0 || timeReturn > 2459){
+        cout << "ERROR! Invalid Time! Re-Enter Time: ";
+        cin  >> timeReturn;
+        cout << endl;
+    }
+}
+
+
+
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+///**************************************************************************************************
+
+/*
+Amount of days on trip: 3
+
+What time was Departure (Military Time): 1300
+What time was the arrival (Military Time): 1600
+Enter cost of round trip: 3000
+Flight cost: 3000 Flight cost reimbursed: 3000
+
+Did you rent a car? (y/n): n
+Was a private vehicle used? (y/n): y
+How many miles were driven? 600
+Car cost is: 348 Car cost reimbursed: 348
+
+Enter cost of conference registration fees: 60
+Conference fees: 60 Conference fees reimbursed: 60
+
+Program ended with exit code: 0
+*/
+
+
+
+
+
+
+
+
+
+
